bed3487a87a47f67d6dd364514b5b94c829fcb1c
Heria021 <hariomsuthar7143@gmail.com>
2025-02-09 12:28:43 +0530

    Convex Removed

--- convex/database.tsx
+++
@@ -1,8 +0,0 @@
-import { query } from "./_generated/server";
-
-export const getAllInvestorsAndMentors = query({
-  args: {},
-  handler: async (ctx) => {
-    return await ctx.db.query("investorsAndMentors").collect();
-  },
-});
